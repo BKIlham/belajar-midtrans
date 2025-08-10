e045b8be55635148f78200fae53cf68b79745cef
BintangKus <bintangkus308@gmail.com>
2025-08-10 16:00:58 +0700

    snap popup seamless

---
+++ app/api/tokenizer/route.js
@@ -0,0 +1,31 @@
+import Midtrans from "midtrans-client";
+import { NextResponse } from "next/server";
+
+let snap = new Midtrans.Snap({
+    isProduction: false,
+    serverKey : process.env.SECRET,
+    clientKey : process.env.NEXT_PUBLIC_CLIENT
+});
+
+export async function POST(request) {
+    const {id, productName, price, quantity} = await request.json();
+    
+    let parameter = {
+        item_details: {
+            name : productName,
+            price : price,
+            quantity : quantity
+        },
+        transaction_details : {
+            order_id : id,
+            gross_amount : price * quantity
+        },
+        callbacks: {
+          finish: "https://1ebcc0f34024.ngrok-free.app/thanks"
+        }
+    }
+    
+    const token = await snap.createTransactionToken(parameter);
+    console.log(token);
+    return NextResponse.json({token});
+}
